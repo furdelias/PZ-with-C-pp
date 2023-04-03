541f59d9d92e3690b7a52cadfe94fca8f74ca195
furdelias <91177145+furdelias@users.noreply.github.com>
2023-04-03 17:22:10 +0300

    Create pz-4.cpp

---
+++ pz-4.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+bool isLeap(int year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
+}
